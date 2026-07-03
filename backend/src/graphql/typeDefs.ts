7d257a8af33d3472c7e99e4dfd154f8779b071ff
Lucas Vieira <lucasvieiramoura@gmail.com>
2026-07-03 14:08:30 -0300

    Criação dos TypedDef e alterção de biblioteca do apollo-server-express pelo graphql-tag

---
+++ backend/src/graphql/typeDefs.ts
@@ -0,0 +1,91 @@
+import { gql } from 'graphql-tag';
+
+export const typeDefs = gql`
+    enum Role {
+        ADMIN
+        EMPRESA
+        CLIENTE
+        ENTREGADOR
+    }
+
+    enum OrderStatus {
+        PENDING
+        PAID
+        PREPARING
+        READY
+        DELIVERING
+        DELIVERED
+        CANCELED
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        role: Role!
+        address: Address
+        phone: String
+    }
+
+    type Address {
+        cep: String!
+        street: String!
+        aditional: String
+        number: String!
+        lat: Float!
+        long: Float!
+    }
+
+    type Product {
+        id: ID!
+        name: String!
+        description: String
+        price: Float!
+        stock_quantity: Int!
+        ingredients: [String!]!
+    }
+
+    type Order {
+        id: ID!
+        client_id: ID!
+        items: [CartItemOutput!]!
+        total_price: Float!
+        statsus: OrderStatus!
+        payment_id: String
+        driver_location: Location
+        created_at: String!
+    }
+
+    type CartItemOutput {
+        product_id: ID!
+        quantity: Int!
+    }
+
+    input CartItemInput {
+        product_id: ID!
+        quantity: Int!
+    }
+
+    # Operaçãoes de Leitura (Substiu os GETs)
+    type Query {
+        listProducts: [Product!]!
+        trackOrder(orderId: ID!): Order!
+        getDashboardMetrics: DashboardMetrics!
+    }
+
+    type DashboardMetrics {
+        total_revenue: Float!
+        total_orders: Int!
+        low_stock_products: [Product!]!
+    }
+
+    # Operações de Escrita (Substitui os POST, PUT e DELETE)
+    type Mutation {
+        registerUser(name: String!, email: String!, password_hash: String!, role: Role!): String!
+        loginUser(email: String!, password_hash: String!): String!
+        createProduct(name: String!, price: Float!, stock_quantity: Int!, ingredients: [String!]!): Product!
+        checkoutOrder(items: [CartItemInput!]!, total_price: Float!): Order!
+        updateDriverLocation(orderId: ID!, lat: Float!, long: Float!): String!
+        updateOrderStatus(orderId: ID!, status: OrderStatus!): String!
+    }
+`;
